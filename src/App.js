8f31a411121ade8bda738890781181c1c3abeff2
poornesh-pH <poornesh9626@gmail.com>
2022-01-17 20:21:24 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AppRouter from './AppRouter'
 export default function App() {
   return (
     <div>
-    {/* <AppRouter /> */}
+    <AppRouter />
     </div>
   );
 }
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import {Switch,Route} from 'react-router-dom'
-import Dashboard from './Components/Dashboard'
-import App from './App'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
+import App from './App';
 
-const AppRouter = ()=>{
-  return(
+const AppRouter = () => {
+  return (
     <div>
-    <Switch>
-      {/* <Route exact path="/" children={<App />}/> */}
-      {/* <Route exact path="/SNitem1" component={<Dashboard />}/>
-      <Route exact path="/SNitem2" component={<Dashboard />}/>
-      <Route exact path="/SNitem3" component={<Dashboard />}/> */}
-    </Switch>
+      <div>AppRouter</div>
+      <BrowserRouter>
+        <Switch>
+          <Route  path="/" component={Dashboard}/>
+        </Switch>
+      </BrowserRouter>
     </div>
-  )
-}
-export default AppRouter
+  );
+};
+export default AppRouter;

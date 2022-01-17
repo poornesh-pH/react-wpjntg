9ff33db8b456a91729d9aca275bad6516f4df294
poornesh-pH <poornesh9626@gmail.com>
2022-01-17 20:09:35 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./style.css";
 import Dashboard from './Components/Dashboard'
 import Sample from './Sample'
+import AppRouter from './AppRouter'
 export default function App() {
   return (
     <div>
-      <Dashboard />
-      <Sample />
+    {/* <AppRouter /> */}
     </div>
   );
 }
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Dashboard from './Components/Dashboard';
-import App from './App';
+import React from 'react'
+import {Switch,Route} from 'react-router-dom'
+import Dashboard from './Components/Dashboard'
+import App from './App'
 
-const AppRouter = () => {
-  return (
+const AppRouter = ()=>{
+  return(
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" children={<App />} />
-          {/* <Route exact path="/SNitem1" component={<Dashboard />}/>
+    <Switch>
+      {/* <Route exact path="/" children={<App />}/> */}
+      {/* <Route exact path="/SNitem1" component={<Dashboard />}/>
       <Route exact path="/SNitem2" component={<Dashboard />}/>
       <Route exact path="/SNitem3" component={<Dashboard />}/> */}
-        </Switch>
-      </BrowserRouter>
+    </Switch>
     </div>
-  );
-};
-export default AppRouter;
+  )
+}
+export default AppRouter
